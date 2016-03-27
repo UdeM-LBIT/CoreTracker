e5d5c03c90c8080f63e721d2115a74c568bb212d
maclandrol <emmanuel.noutahi@hotmail.ca>
2016-03-27 04:35:46 -0400

    -m

--- setup.py
+++ setup.py
@@ -6,56 +6,75 @@
 #   python setup.py build
 #   python setup.py install
 #
-
-import setuptools
+from __future__ import division, absolute_import, print_function
 import os, sys
-
-if os.path.exists('README.md'):
-	README = open('README.md').read()
-else:
-	README = ""  # a placeholder, readme is generated on release
-
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), "python")))
+from numpy.distutils.core  import Extension
+import setuptools
 from coretracker import __project__, __version__
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), "python")))
 
-setuptools.setup(
-	name=__project__,
-	version=__version__,
+def configuration(top_path=None) :
 
-	description="CoreTracker, A codon reassignment tracker",
-	url='https://github.com/UdeM-LBIT/CoreTracker',
-	author='Emmanuel Noutahi',
-	author_email='fmr.noutahi@umontreal.ca',
-	scripts = ['scripts/coretracker', 'scripts/translate'],
+    fisher_exact = Extension(name='coretracker.FisherExact.statlib.fexact', sources=[os.path.join(top_path, 'coretracker/FisherExact/statlib', 'FEXACT.F90')])
+    asa159 = Extension(name='coretracker.FisherExact.statlib.asa159', sources=[os.path.join(top_path, 'coretracker/FisherExact/statlib', 'asa159.f90')])
+    asa205 = Extension(name='coretracker.FisherExact.statlib.asa205', sources=[os.path.join(top_path, 'coretracker/FisherExact/statlib', 'asa205.f90')])
+    return [fisher_exact, asa205, asa159]
 
-	packages=setuptools.find_packages(exclude=['tests']),
 
-	entry_points={'console_scripts': []},
 
-	keywords="bioinformatics codon reassignment tracker",
+def setup_package():
+    if os.path.exists('README.md'):
+        README = open('README.md').read()
+    else:
+        README = ""  # a placeholder, readme is generated on release
+    print("\nVersion : %s\n"%__version__)
 
-	long_description=(README + '\n'),
-	license='MIT',
-	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Environment :: Console',
-		'Natural Language :: English',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Education',
-		'Intended Audience :: Science/Research',
-		'License :: OSI Approved :: GNU General Public License (GPL)',
-		'Operating System :: POSIX',
-		'Programming Language :: Python',
-	],
-	install_requires=[
-		'ete3',
-		'numpy >= 1.8.1',
-		'pandas',
-		'scikit-learn',
-		'scipy',
-		'biopython',
-		'matplotlib',
-		'WeasyPrint',
-		'PyYAML',
-	]
-)
+    from numpy.distutils.core import setup
+    fortran_extnsion = configuration(os.path.dirname(__file__))
+
+    setup(
+        name=__project__,
+        version=__version__,
+        maintainer='UdeM-LBIT',
+        description="CoreTracker, A codon reassignment tracker",
+        url='https://github.com/UdeM-LBIT/CoreTracker',
+        author='Emmanuel Noutahi',
+        author_email='fmr.noutahi@umontreal.ca',
+        scripts = ['bin/coretracker', 'bin/translate', 'bin/fusion'],
+
+        entry_points={'console_scripts': []},
+
+        keywords="bioinformatics codon reassignment tracker",
+
+        long_description=(README + '\n'),
+        license='MIT',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Natural Language :: English',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+        ],
+
+        setup_requires=['numpy'],
+        install_requires = [
+            'ete3',
+            'numpy >= 1.8.1',
+            'pandas',
+            'scikit-learn',
+            'scipy',
+            'biopython',
+            'matplotlib',
+            'WeasyPrint',
+            'PyYAML',
+            'PyQt4',
+        ],
+        ext_modules = fortran_extnsion
+    )
+
+if __name__ == '__main__':
+    setup_package()
